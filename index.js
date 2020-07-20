6518383349eae5059d59e744905494c20d53d019
abdulloooh <abdullahakinwumi@gmail.com>
2020-07-20 11:51:16 +0100

    init commit, basic routes created for genres

---
+++ index.js
@@ -0,0 +1,63 @@
+const Joi = require("@hapi/joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "Romance" },
+];
+
+app.get("/api/genres", (req, res) => res.send(genres));
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found");
+  res.send(genre);
+});
+
+app.post("/api/genres", (req, res) => {
+  const { error } = genreValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+
+  genres.push(genre);
+
+  res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found");
+
+  const { error } = genreValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found");
+
+  genres.splice(genres.indexOf(genre), 1);
+
+  res.send(genre);
+});
+
+function genreValidation(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
