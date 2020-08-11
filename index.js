48952a4d170940aa44546dc5e1d186e963ef1662
abdulloooh <abdullahakinwumi@gmail.com>
2020-08-11 12:04:48 +0100

    corredted CRUD code for vidly

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const genres = require("./routes/genres");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongodb successfully"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use("/api/genres", genres);
